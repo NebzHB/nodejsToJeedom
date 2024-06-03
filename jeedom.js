bcc0c10c1c346ec344aee227114251d9bb4a0030
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-06-03 12:14:27 +0200

    Update jeedom.js

--- jeedom.js
+++ jeedom.js
@@ -100,7 +100,7 @@ module.exports = ( type, url, apikey, logLevel, mode="event" ) => {
 	thisLogLevel=logLevel;
 	thisMode=mode; // "jsonrpc" | "event"
 	axiosInstance = axios.create({
-		timeout: 10000,
+		timeout: 15000,
 		headers: {'Accept-Encoding': 'gzip, deflate'},
 	});
 	return sendToJeedom;
