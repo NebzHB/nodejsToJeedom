f864d31d568f95a5b1a77e5602643e48e5053ed4
Nebz <28622481+NebzHB@users.noreply.github.com>
2024-10-21 11:25:54 +0200

    Update jeedom.js

--- jeedom.js
+++ jeedom.js
@@ -86,7 +86,7 @@ class JeedomAPI {
         data.type = 'event';
         data.apikey = this.apikey;
         data.plugin = this.type;
-        const message = { data: data, tryCount: 0, isJSONRPC: isJSONRPC };
+        const message = {data: data, tryCount: 0, isJSONRPC: isJSONRPC};
 
         if (this.logLevel === 'ultradebug') {
             console.log("Ajout du message " + JSON.stringify(message) + " dans la queue des messages à transmettre à Jeedom");
